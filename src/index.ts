0e802a09cfc7c40efd5196c071cd12614f173e00
Not-Jayden <jaydenaus9@gmail.com>
2023-11-24 00:27:08 +0800

    attempt to fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
+import { event } from './action';
+import {
+	once,
+	preventDefault,
+	self,
+	stopImmediatePropagation,
+	stopPropagation,
+	trusted,
+	withModifiers,
+} from './modifiers';
+
 export { event } from './action';
-export * from './modifiers';
+export { once, preventDefault, self, stopImmediatePropagation, stopPropagation, trusted, withModifiers };
